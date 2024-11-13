26badd79cb35782e5832de3eb3a95237ac075460
Royvanheeswijk <539880@student.fontys.nl>
2024-11-13 08:34:54 +0100

    setup document voor stage website
    document voor website overgenomen van maandag website en deze ga ik opnieuw gebruiken om mijn stage website voor te maken

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+export default function Home() {
+  return (
+    
+    <body>
+      <div>
+        <header class="grid">
+          <h1>12-Col Grid</h1>
+          <nav>
+            <a href="">About me</a>
+            <a href="">Projects</a>
+          </nav> 
+        </header>
+        <main class="grid">
+         
+        </main>
+      </div>
+    </body>
+  
+  );
+}
